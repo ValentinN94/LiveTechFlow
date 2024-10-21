49beecf2a1a683bd2604d150b7304ba19ab633d3
ValentinN94 <negruvalentin94@gmail.com>
2024-10-21 21:12:15 +0300

    exemplu post

--- hello.py
+++ hello.py
@@ -3,14 +3,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if True:
-            return request.form['username']
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return "<p>Hello</p>"
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if True:
+#             return request.form['username']
+#         else:
+#             error = 'Invalid username/password'
+#     # the code below is executed if the request method
+#     # was GET or the credentials were invalid
+#     return "<p>Hello</p>"
+
+@app.route('/guide', methods=["GET", "POST"])
+def add_guide():
+    title = request.form['title']
+    return '<p>Hello!</p>'
