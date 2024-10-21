6e6c0baa6b25a6d4f44f6a3e0c5749449faf990f
ValentinN94 <negruvalentin94@gmail.com>
2024-10-21 20:56:42 +0300

    prima aplicatie

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if True:
+            return request.form['username']
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return "<p>Hello</p>"
