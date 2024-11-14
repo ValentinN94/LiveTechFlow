139eccab0d8fadf4a189b66babc9729b6741d15b
ValentinN94 <negruvalentin94@gmail.com>
2024-11-14 20:31:40 +0200

    UI + DB integrations

--- flask_auth_app/auth.py
+++ flask_auth_app/auth.py
@@ -2,12 +2,28 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
 
 @auth.route('/signup')
 def signup():
@@ -25,7 +41,7 @@ def signup_post():
         flash('Email address already exists!')
         return redirect(url_for('auth.signup'))
 
-    new_user = User(email=email, password=generate_password_hash(password, method='sha256'), name=name)
+    new_user = User(email=email, password=generate_password_hash(password, method='pbkdf2:sha256'), name=name)
     print('aici e user-ul' + str(new_user))
 
     db.session.add(new_user)
@@ -34,5 +50,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
